7f813c5d56c4c7e7b7d3573444191285e8a2ceef
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2022-09-10 11:21:05 +0800

    Create diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 #echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
 #echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
 #echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
-echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+#echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
 #echo 'src-git video https://github.com/openwrt/video.git' >>feeds.conf.default
 #echo 'src-git targets https://github.com/openwrt/targets.git' >>feeds.conf.default
 #echo 'src-git oldpackages http://git.openwrt.org/packages.git' >>feeds.conf.default
