029a65185acbaef127417de84fb088ace3878716
tgdh520 <59686937+tgdh520@users.noreply.github.com>
2022-09-10 09:43:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@ echo 'src-git video https://github.com/openwrt/video.git' >>feeds.conf.default
 echo 'src-git targets https://github.com/openwrt/targets.git' >>feeds.conf.default
 echo 'src-git oldpackages http://git.openwrt.org/packages.git' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git weifuture https://github.com/hongweifuture/openwrt-packages' >>feeds.conf.default
 echo 'src-git pwdep https://github.com/hongweifuture/pwdep.git' >>feeds.conf.default
 echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
